1c3b9620e81724afe8748f43595ff4a873dc1b04
manon.dauchy <dauchy.manon@hotmail.com>
2022-04-04 14:05:28 +0200

    blefinit

--- app/src/main/java/fr/isen/dauchy/androiderestaurant/ble/BleService.kt
+++ app/src/main/java/fr/isen/dauchy/androiderestaurant/ble/BleService.kt
@@ -6,4 +6,5 @@ import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
 
 class BleService (val name: String, val characteristics: MutableList<BluetoothGattCharacteristic> ):
     ExpandableGroup<BluetoothGattCharacteristic>(name, characteristics){
+
 }
